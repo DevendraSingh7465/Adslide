83802e62ce72fb3e180d1216667956baedfd3448
Devendra Singh <127510525+DevendraSingh7465@users.noreply.github.com>
2024-10-24 18:54:20 +0530

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,10 @@
+<?php
+    $host = "sql205.infinityfree.com";
+    $dbusername = "if0_37517031";
+    $dbpassword = "Zk49dyn5aO";
+    $dbname = "if0_37517031_test";
+    
+    // Create connection
+    $conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+
+?>
